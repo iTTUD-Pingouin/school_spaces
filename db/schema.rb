95d93c6d88d5a073e5167fb9685fdbbe931e63f6
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-21 15:43:51 +0200

    spaces table and model added to project

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_125639) do
+ActiveRecord::Schema.define(version: 2018_05_21_134146) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "spaces", force: :cascade do |t|
+    t.string "city"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_spaces_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -32,4 +40,5 @@ ActiveRecord::Schema.define(version: 2018_05_21_125639) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spaces", "users"
 end
