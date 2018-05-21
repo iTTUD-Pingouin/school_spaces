f63d80a0e7715111e68ad380d192528cd0e6f3bc
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-21 15:46:45 +0200

    reservation model created, and added to DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_134146) do
+ActiveRecord::Schema.define(version: 2018_05_21_134429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.bigint "space_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["space_id"], name: "index_reservations_on_space_id"
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "spaces", force: :cascade do |t|
     t.string "city"
@@ -40,5 +49,7 @@ ActiveRecord::Schema.define(version: 2018_05_21_134146) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "spaces"
+  add_foreign_key "reservations", "users"
   add_foreign_key "spaces", "users"
 end
