d158cf21f50f28cbfcc7b4ce1eb4537229d3b5c0
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-21 15:39:49 +0200

    add owners table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_21_125639) do
+ActiveRecord::Schema.define(version: 2018_05_21_132555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "owners", force: :cascade do |t|
+    t.string "first_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
