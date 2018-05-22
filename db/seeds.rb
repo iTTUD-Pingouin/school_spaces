c8f1b2210cce934b6e62ec87e5f2d383c348040e
Virgile Pradel <virgile.pradel@gmail.com>
2018-05-22 14:18:31 +0200

    Patch. Now seed is lighter. 5 users book 3 different spaces

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,12 +8,12 @@
 
 require 'faker'
 
-puts 'Creating 100 fake users and their spaces...'
-  100.times do
+puts 'Creating 20 fake users and their spaces...'
+  20.times do
     user = User.new(
       email: Faker::Internet.email,
       password: "123456",
-      first_name: Faker::Name
+      first_name: Faker::Name.first_name
       )
     user.save!
     space = Space.new(
@@ -24,17 +24,28 @@ puts 'Creating 100 fake users and their spaces...'
   end
 puts 'Fake users and corresponding spaces : done!'
 
-puts 'Creating 100 fake reservations'
-  mock_user_id = 100
+puts 'Creating 15 fake reservations'
+  mock_user_id = 20
   mock_space_id = 1
-  30.times do
+  5.times do
+    mock_space_id_2 = mock_space_id + 1
+    mock_space_id_3 = mock_space_id + 2
     reservation = Reservation.new(
       user_id: mock_user_id,
       space_id: mock_space_id
       )
     reservation.save!
+    reservation_2 = Reservation.new(
+      user_id: mock_user_id,
+      space_id: mock_space_id_2
+      )
+    reservation_2.save!
+    reservation_3 = Reservation.new(
+      user_id: mock_user_id,
+      space_id: mock_space_id_3
+      )
+    reservation_3.save!
     mock_user_id -= 1
     mock_space_id += 1
   end
-
 puts 'Finished! Wubbalubbadubdub Mfs !'
