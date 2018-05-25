fe1f192dee2fba475e0219d75e241ac65e68462d
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-25 12:54:09 +0200

    improve datepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,9 +2,15 @@ import "bootstrap";
 
 
 import flatpickr from "flatpickr";
-import 'flatpickr/dist/flatpickr.min.css'
+import 'flatpickr/dist/themes/material_blue.css'
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 
-flatpickr(".datepicker", {
 
-});
+// flatpickr(".datepicker", {
+//   mode: 'range',
+// });
+flatpickr("#range_start", {
+  // altInput: true,
+  plugins: [new rangePlugin({ input: "#range_end"})]
+})
