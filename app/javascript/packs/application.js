637fc251767db8dcea23eae9fc06a9bfba3d01dc
Guillaume Dutti <guillaume.dutti@live.fr>
2018-05-25 16:33:35 +0200

    fix bug

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,5 @@ import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#reservation_start_date", {
   plugins: [new rangePlugin({ input: "#reservation_end_date"})]
-  dateFormat: "d-m-Y"
 })
 
